60ea72175f0882468cbed0bef4ae228da3a00a0c
rameshjdev <rameshjdev01@gmail.com>
2020-09-13 20:27:55 -0500

    Model changes

--- src/main/java/com/centene/codechallenge/model/Dependent.java
+++ src/main/java/com/centene/codechallenge/model/Dependent.java
@@ -1,40 +1,52 @@
 package com.centene.codechallenge.model;
 
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 //@Document("enrollee")
 @Entity
 @Table(name = "dependent")
 public class Dependent {
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
     @Column
+    @NotBlank(message = "DOB is mandatory")
     private String dob;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "enrollee_id", nullable = false)
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name="enrollee_id", nullable=false)
     @JsonBackReference
     private Enrollee enrollee;
 
 
+
     public long getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getName() {
+	public String getName() {
         return name;
     }
 
@@ -50,13 +62,35 @@ public class Dependent {
         this.dob = dob;
     }
 
-    public void setEnrollee(Enrollee enrollee) {
-        this.enrollee = enrollee;
-    }
+	public void setEnrollee(Enrollee enrollee) {
+		this.enrollee = enrollee;
+	}
 
-    public Enrollee getEnrollee() {
-        return enrollee;
-    }
+	public Enrollee getEnrollee() {
+		return enrollee;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dependent other = (Dependent) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 
 }
--- src/main/java/com/centene/codechallenge/model/Enrollee.java
+++ src/main/java/com/centene/codechallenge/model/Enrollee.java
@@ -1,35 +1,48 @@
 package com.centene.codechallenge.model;
 
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import javax.persistence.*;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 //@Document("enrollee")
 @Entity
 @Table(name = "enrollee")
 public class Enrollee {
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
     @Column
     private boolean activationStatus;
 
     @Column
+    @NotBlank(message = "DOB is mandatory")
     private String dob;
 
+    @Pattern(regexp = "([0-9]{10})")
     @Column
     private String phoneNumber;
 
     @OneToMany(mappedBy = "enrollee", cascade = {
             CascadeType.ALL
-    })
+        })
     @JsonManagedReference
     private List<Dependent> dependents;
 
@@ -74,12 +87,36 @@ public class Enrollee {
         this.phoneNumber = phoneNumber;
     }
 
-    public List<Dependent> getDependents() {
-        return dependents;
-    }
+	public List<Dependent> getDependents() {
+		return dependents;
+	}
 
-    public void setDependents(List<Dependent> dependents) {
-        this.dependents = dependents;
-    }
+	public void setDependents(List<Dependent> dependents) {
+		this.dependents = dependents;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enrollee other = (Enrollee) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
 
 }
